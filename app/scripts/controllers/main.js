68a45f10f502d341643fc1492f8da3703907f6cf
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-19 18:18:11 +0200

    Init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngResourceHackApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
